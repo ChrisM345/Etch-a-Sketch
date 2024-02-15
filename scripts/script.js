80a59cf526440c4064a195475712cab4711a84f0
ChrisM345 <chrism.12277@gmail.com>
2024-02-14 19:52:50 -0600

    Added some basic CSS and code cleanup

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 container = document.querySelector('.container');
 
+//Random RGB value using Math.floor with Math.random
 function randomColor() {
     r = Math.floor(Math.random() * 256);
     g = Math.floor(Math.random() * 256);
     b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`
 }
+
+//Create the grid. Adjust size of squares according to the number entered so it always fills a 960px by 960px area.
 function createGrid(squares){
     gridSize = 960 / squares;
     gridSize = gridSize + 'px';
@@ -20,22 +23,18 @@ function createGrid(squares){
             gridCol.appendChild(grid)
         }
     }
-    //grid.style.backgroundColor = 'pink';
+    //Select all grid squares and add an event listener to them which changes the color with mouseover.
     const gridSquares = document.querySelectorAll('.grid');
     gridSquares.forEach(item => {
         item.addEventListener('mouseover', (e) => {
-            //console.log(e)
-            //console.log(item)
             item.style.backgroundColor = randomColor();
-            //item.style.outline = 'none';
         })
     })
 }
 
+//prompts the user to create a new grid and deletes the old grid.
 function promptNewGrid(){
-    //console.log("prompted")
     var numberOfSquares = prompt("Please enter the number of squares per side for the new grid");
-    //console.log(typeof(numberOfSquares));
     if (!isNaN(numberOfSquares)){
         parent = document.querySelector('.container')
         while (parent.hasChildNodes()) {
@@ -47,4 +46,5 @@ function promptNewGrid(){
 const newGridButton = document.querySelector('.create-grid');
 newGridButton.addEventListener('click', promptNewGrid);
 
+//Starting grid. 16 by 16
 createGrid(16)
